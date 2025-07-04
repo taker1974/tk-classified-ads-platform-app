43de37be3e6d5239d0339c51748a64aaf943069f
taker1974 <kostus.online.1974@yandex.ru>
2025-07-04 10:12:47 +0300

    test: need handle validation errors befor rte

--- tk-classified-ads-platform/src/test/java/ru/spb/tksoft/ads/AuthControllerE2ETest.java
+++ tk-classified-ads-platform/src/test/java/ru/spb/tksoft/ads/AuthControllerE2ETest.java
@@ -1,173 +1,128 @@
 package ru.spb.tksoft.ads;
 
-import org.junit.jupiter.api.*;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.spb.tksoft.ads.dto.request.LoginRequestDto;
 import ru.spb.tksoft.ads.dto.request.RegisterRequestDto;
-import ru.spb.tksoft.ads.enumeration.UserRole;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class AuthControllerE2ETest {
+class AuthControllerE2ETest extends E2ETestBase {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+    private final RegisterRequestDto goodRegisterRequestDto = createValidRegisterRequest();
 
-    @LocalServerPort
-    private int port;
-
-    private String baseUrl;
-
-    @BeforeEach
-    void setUp() {
-        baseUrl = "http://localhost:" + port;
-    }
-
-    @Test
-    void register_shouldReturn201_whenValidRequest() {
-        // Arrange
+    private ResponseEntity<Void> registerOk() {
         RegisterRequestDto request = createValidRegisterRequest();
-        
-        // Act
-        ResponseEntity<Void> response = sendRegisterRequest(request);
-        
-        // Assert
-        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        return sendRegisterRequest(request);
     }
 
+    @DisplayName("Registered - 201, otherwise - 400 or whatever")
     @Test
-    void login_shouldReturn200_whenValidCredentials() {
-        // Arrange
-        RegisterRequestDto registerRequest = createValidRegisterRequest();
-        sendRegisterRequest(registerRequest);
-        
-        LoginRequestDto loginRequest = new LoginRequestDto(
-            "valid@example.com", 
-            "validPassword123"
-        );
+    void register_201_whenValidRequest() {
+        Assertions.assertEquals(HttpStatus.CREATED, registerOk().getStatusCode());
+    }
 
-        // Act
+    @DisplayName("Logged in - 200, otherwise - 400 or whatever")
+    @Test
+    void login_200_whenValidCredentials() {
+
+        registerOk();
+
+        var loginRequest = new LoginRequestDto(
+                goodRegisterRequestDto.getUsername(),
+                goodRegisterRequestDto.getPassword());
+
         ResponseEntity<Void> response = sendLoginRequest(loginRequest);
-        
-        // Assert
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
-    // // Параметризованные тесты для невалидных данных регистрации
-    // static Stream<RegisterRequestDto> invalidRegisterRequests() {
-    //     return Stream.of(
-    //         new RegisterRequestDto("invalid", "pwd", "John", "Doe", "+79991234567", UserRole.USER),
-    //         new RegisterRequestDto("a@b", "pwd", "John", "Doe", "+79991234567", UserRole.USER),
-    //         new RegisterRequestDto("valid@example.com", "", "John", "Doe", "+79991234567", UserRole.USER),
-    //         new RegisterRequestDto("valid@example.com", "short", "John", "Doe", "+79991234567", UserRole.USER),
-    //         new RegisterRequestDto("valid@example.com", "password123", "", "Doe", "+79991234567", UserRole.USER),
-    //         new RegisterRequestDto("valid@example.com", "password123", "J", "Doe", "+79991234567", UserRole.USER),
-    //         new RegisterRequestDto("valid@example.com", "password123", "John", "Doe", "invalid_phone", UserRole.USER),
-    //         new RegisterRequestDto(null, null, null, null, null, null)
-    //     );
-    // }
+    static Stream<RegisterRequestDto> invalidRegisterRequests() {
+        return Stream.of(
+                new RegisterRequestDto("inv", "pwd", "John", "Doe", "+79991234567"),
+                new RegisterRequestDto("a@b", "pwd", "John", "Doe", "+79991234567"),
+                new RegisterRequestDto("valid@example.com", "", "John", "Doe", "+79991234567"),
+                new RegisterRequestDto("valid@example.com", "short", "John", "Doe", "+79991234567"),
+                new RegisterRequestDto("valid@example.com", "pass1111", "", "Doe", "+79991234567"),
+                new RegisterRequestDto("valid@example.com", "pass1111", "J", "Doe", "+79991234567"),
+                new RegisterRequestDto("valid@example.com", "pass1111", "John", "Doe", "phone"),
+                new RegisterRequestDto(null, null, null, null, null, null));
+    }
 
-    // @ParameterizedTest
-    // @MethodSource("invalidRegisterRequests")
-    // void register_shouldReturn400_whenInvalidRequest(RegisterRequestDto invalidRequest) {
-    //     // Act
-    //     ResponseEntity<Void> response = sendRegisterRequest(invalidRequest);
-        
-    //     // Assert
-    //     Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-    // }
+    @DisplayName("Register failed - 400")
+    @ParameterizedTest
+    @MethodSource("invalidRegisterRequests")
+    void register_400_whenInvalidRequest(RegisterRequestDto invalidRequest) {
+
+        ResponseEntity<Void> response = sendRegisterRequest(invalidRequest);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
 
     // // Параметризованные тесты для невалидных данных аутентификации
     // static Stream<LoginRequestDto> invalidLoginRequests() {
-    //     return Stream.of(
-    //         new LoginRequestDto("invalid", "password"),
-    //         new LoginRequestDto("a@b", "password"),
-    //         new LoginRequestDto("valid@example.com", ""),
-    //         new LoginRequestDto("valid@example.com", "short"),
-    //         new LoginRequestDto(null, null)
-    //     );
+    // return Stream.of(
+    // new LoginRequestDto("invalid", "password"),
+    // new LoginRequestDto("a@b", "password"),
+    // new LoginRequestDto("valid@example.com", ""),
+    // new LoginRequestDto("valid@example.com", "short"),
+    // new LoginRequestDto(null, null)
+    // );
     // }
 
     // @ParameterizedTest
     // @MethodSource("invalidLoginRequests")
     // void login_shouldReturn400_whenInvalidRequest(LoginRequestDto invalidRequest) {
-    //     // Act
-    //     ResponseEntity<Void> response = sendLoginRequest(invalidRequest);
-        
-    //     // Assert
-    //     Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    // // Act
+    // ResponseEntity<Void> response = sendLoginRequest(invalidRequest);
+
+    // // Assert
+    // Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     // }
 
     // @Test
     // void register_shouldReturn400_whenUsernameExists() {
-    //     // Arrange
-    //     RegisterRequestDto firstUser = createValidRegisterRequest();
-    //     sendRegisterRequest(firstUser);
-        
-    //     RegisterRequestDto secondUser = new RegisterRequestDto(
-    //         "valid@example.com",
-    //         "anotherPassword",
-    //         "Second",
-    //         "User",
-    //         "+7(999)222-22-22",
-    //         UserRole.USER
-    //     );
+    // // Arrange
+    // RegisterRequestDto firstUser = createValidRegisterRequest();
+    // sendRegisterRequest(firstUser);
 
-    //     // Act
-    //     ResponseEntity<Void> response = sendRegisterRequest(secondUser);
-        
-    //     // Assert
-    //     Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    // RegisterRequestDto secondUser = new RegisterRequestDto(
+    // "valid@example.com",
+    // "anotherPassword",
+    // "Second",
+    // "User",
+    // "+7(999)222-22-22",
+    // UserRole.USER
+    // );
+
+    // // Act
+    // ResponseEntity<Void> response = sendRegisterRequest(secondUser);
+
+    // // Assert
+    // Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     // }
 
     // @Test
     // void login_shouldReturn401_whenInvalidCredentials() {
-    //     // Arrange
-    //     RegisterRequestDto registerRequest = createValidRegisterRequest();
-    //     sendRegisterRequest(registerRequest);
-        
-    //     LoginRequestDto loginRequest = new LoginRequestDto(
-    //         "valid@example.com", 
-    //         "wrongPassword"
-    //     );
+    // // Arrange
+    // RegisterRequestDto registerRequest = createValidRegisterRequest();
+    // sendRegisterRequest(registerRequest);
 
-    //     // Act
-    //     ResponseEntity<Void> response = sendLoginRequest(loginRequest);
-        
-    //     // Assert
-    //     Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    // LoginRequestDto loginRequest = new LoginRequestDto(
+    // "valid@example.com",
+    // "wrongPassword"
+    // );
+
+    // // Act
+    // ResponseEntity<Void> response = sendLoginRequest(loginRequest);
+
+    // // Assert
+    // Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     // }
-
-    private RegisterRequestDto createValidRegisterRequest() {
-
-        return new RegisterRequestDto(
-            "valid@example.com",
-            "validPassword123",
-            "Johnadfsdfs",
-            "DoeAdsds",
-            "+79991234567",
-            UserRole.USER
-        );
-    }
-
-    private ResponseEntity<Void> sendRegisterRequest(RegisterRequestDto request) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<RegisterRequestDto> entity = new HttpEntity<>(request, headers);
-        return restTemplate.postForEntity(baseUrl + "/register", entity, Void.class);
-    }
-
-    private ResponseEntity<Void> sendLoginRequest(LoginRequestDto request) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<LoginRequestDto> entity = new HttpEntity<>(request, headers);
-        return restTemplate.postForEntity(baseUrl + "/login", entity, Void.class);
-    }
 }
---
+++ tk-classified-ads-platform/src/test/java/ru/spb/tksoft/ads/E2ETestBase.java
@@ -0,0 +1,75 @@
+package ru.spb.tksoft.ads;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.*;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import ru.spb.tksoft.ads.dto.request.LoginRequestDto;
+import ru.spb.tksoft.ads.dto.request.RegisterRequestDto;
+import ru.spb.tksoft.ads.enumeration.UserRole;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class E2ETestBase {
+
+    @LocalServerPort
+    protected Integer port;
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    protected String getBaseUrl() {
+        return "http://localhost:" + port;
+    }
+
+    @Container
+    @ServiceConnection
+    protected static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.5"))
+                    .withDatabaseName("tk_ads")
+                    .withUsername("ads_god")
+                    .withPassword("87654321")
+                    .withExposedPorts(5432)
+                    .withReuse(true);
+
+    @DynamicPropertySource
+    protected static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.ads.jdbc-url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.ads.username", postgres::getUsername);
+        registry.add("spring.datasource.ads.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+    }
+
+    protected RegisterRequestDto createValidRegisterRequest() {
+        return new RegisterRequestDto(
+                "valid@example.com",
+                "validPassword123",
+                "John",
+                "Doe",
+                "+79991234567",
+                UserRole.USER);
+    }
+
+    protected ResponseEntity<Void> sendRegisterRequest(RegisterRequestDto request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<RegisterRequestDto> entity = new HttpEntity<>(request, headers);
+        return restTemplate.postForEntity(getBaseUrl() + "/register", entity, Void.class);
+    }
+
+    protected ResponseEntity<Void> sendLoginRequest(LoginRequestDto request) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<LoginRequestDto> entity = new HttpEntity<>(request, headers);
+        return restTemplate.postForEntity(getBaseUrl() + "/login", entity, Void.class);
+    }
+}
